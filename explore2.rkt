c3761244c5db24ba00058c61fe7810331f15d509
sberube484 <sean_berube@student.uml.edu>
2016-03-23 11:28:13 -0400

    Added files via upload

--- explore2.rkt
+++ explore2.rkt
@@ -4,6 +4,7 @@
  (require images/icons/misc)
  (require images/icons/stickman)
  (require images/logos)
+ (require images/flomap)
 
 (require pict images/icons/control images/icons/style)
  (pict->bitmap
@@ -24,3 +25,23 @@
     (running-stickman-icon t #:body-color "pink" #:head-color "red" #:arm-color "purple" #:height 40))
 
 (planet-logo)
+
+(define seanmap
+    (draw-flomap
+     (λ (fm-dc)
+       (send fm-dc set-alpha 0)
+       (send fm-dc set-background "black")
+       (send fm-dc clear)
+       (send fm-dc set-alpha 1/3)
+       (send fm-dc translate 2 2)
+       (send fm-dc set-pen "purple" 4 'long-dash) ;;affects outline of diagram
+       (send fm-dc set-brush "red" 'solid) ;;drawing and picking the colors for 3 ellipses
+       (send fm-dc draw-ellipse 0 0 192 192)
+       (send fm-dc set-brush "green" 'solid)
+       (send fm-dc draw-ellipse 64 0 192 192)
+       (send fm-dc set-brush "blue" 'solid)
+       (send fm-dc draw-ellipse 32 44 192 192)
+       (send fm-dc set-brush "orange" 'solid)
+       (send fm-dc draw-ellipse 84 44 192 192))
+     260 240))
+> (flomap->bitmap seanmap)
